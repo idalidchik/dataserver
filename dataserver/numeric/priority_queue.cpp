300d3fd9f99a1982d7b89b71a69e2cb52f2e8805
idalidchik <idalidchik@gmail.com>
2019-05-31 20:43:35 +0200

    priority_queue

---
+++ dataserver/numeric/priority_queue.cpp
@@ -0,0 +1,76 @@
+// priority_queue.cpp
+#include "dataserver/numeric/priority_queue.h"
+#include <algorithm>
+#include <numeric>
+
+#if SDL_DEBUG
+namespace sdl { namespace {
+
+    template<class T>
+    inline bool is_sorted(T const & result) {
+        return std::is_sorted(std::begin(result), std::end(result));
+    }
+
+    template<class T>
+    inline bool is_sorted_desc(T const & result) {
+        return std::is_sorted(std::begin(result), std::end(result),
+            [](auto const & x, auto const & y) {
+            return y < x;
+        });
+    }
+    class unit_test {
+        void test_queue(bool descending);
+    public:
+        unit_test() {
+            test_queue(false);
+            test_queue(true);
+        }
+    };
+    void unit_test::test_queue(const bool descending)
+    {
+        struct cost_value {
+            size_t m_weight = 0;
+            int priority = 0;
+            size_t weight() const {
+                return m_weight;
+            }
+        };
+        using data_array = std::vector<cost_value>;
+        using queue_type = priority_queue<int, data_array>;
+        enum { N = 10 };
+        enum { trace = 0 };
+        data_array data(N);
+        for (size_t i = 0; i < data.size(); ++i) {
+            data[i].m_weight = descending ? (data.size() - i) : i;
+        }
+        queue_type test(data);
+        for (size_t i = 0; i < data.size(); ++i) {
+            test.insert((queue_type::value_type) i);
+        }
+        SDL_ASSERT(test.size() == data.size());
+        {
+            std::vector<size_t> result;
+            size_t i = 0;
+            while (!test.empty()) {
+                ++i;
+                result.push_back(test.getmin());
+                SDL_ASSERT(result.size() == i);
+                SDL_ASSERT(test.size() == data.size() - i);
+                if (trace) {
+                    std::cout << result.back() << " ";
+                }
+            }
+            SDL_ASSERT(i == data.size());
+            SDL_ASSERT(result.size() == data.size());
+            SDL_ASSERT(descending || is_sorted(result));
+            SDL_ASSERT(!descending || is_sorted_desc(result));
+        }
+        if (trace) {
+            std::cout << std::endl;
+        }
+    }
+    static unit_test s_test;
+
+}} // sdl
+#endif //#if SDL_DEBUG
+
---
+++ dataserver/numeric/priority_queue.h
@@ -0,0 +1,142 @@
+// priority_queue.h
+//
+#pragma once
+#ifndef __SDL_NUMERIC_PRIORITY_QUEUE_H__
+#define __SDL_NUMERIC_PRIORITY_QUEUE_H__
+
+#include "dataserver/common/common.h"
+#include <vector>
+#include <deque>
+
+namespace sdl {
+
+template<typename T, typename U, class Container = std::vector<T> >
+class priority_queue: sdl::noncopyable {  
+    using data_array = U;
+    data_array & m_qp;  // weight & priority index
+    Container m_pq;     // priority queue as binary heap, minimum weight is on top
+    static void reserve(std::deque<T> & v, size_t size) {}
+    template<typename vector_type>
+    static void reserve(vector_type & v, size_t size) { 
+        v.reserve(size);
+    }
+public:
+    using value_type = T;
+    explicit priority_queue(data_array & m)
+        : m_qp(m)
+        , m_pq(1) // m_pq[0] reserved
+    {
+        A_STATIC_CHECK_NOT_TYPE(void, std::declval<typename data_array::value_type>().weight());
+        A_STATIC_CHECK_NOT_TYPE(void, std::declval<typename data_array::value_type>().priority);
+        SDL_ASSERT(empty());
+        reserve(m_pq, 64);
+    }
+    size_t size() const {
+        SDL_ASSERT(!m_pq.empty());
+        return m_pq.size() - 1;
+    }
+    bool empty() const {
+        return 0 == size();
+    }
+    value_type top() const {
+        SDL_ASSERT(!empty());
+        return m_pq[1];
+    }
+    value_type getmin();
+    void insert(value_type);
+    void lower(value_type);
+private:
+#if SDL_DEBUG
+    bool check_index(const size_t i) const {
+        SDL_ASSERT(i >= 0);
+        SDL_ASSERT(i < m_pq.size());
+        SDL_ASSERT(m_pq.size() < (size_t) std::numeric_limits<value_type>::max());
+        return true;
+    }
+#endif
+    bool less(size_t i, size_t j) const;
+    void exch(size_t i, size_t j);
+    void fixUp(size_t k);
+    void fixDown(size_t k, size_t N);
+};
+
+template<typename T, typename U, class Container>
+bool priority_queue<T, U, Container>::less(const size_t i, const size_t j) const {
+    SDL_ASSERT(check_index(i));
+    SDL_ASSERT(check_index(j));
+    const auto & pi = m_qp[m_pq[i]];
+    const auto & pj = m_qp[m_pq[j]];
+    return pi.weight() < pj.weight();
+}
+
+template<typename T, typename U, class Container>
+void priority_queue<T, U, Container>::exch(const size_t i, const size_t j)
+{
+    SDL_ASSERT(check_index(i));
+    SDL_ASSERT(check_index(j));
+    std::swap(m_pq[i], m_pq[j]);
+    auto & pi = m_qp[m_pq[i]];
+    auto & pj = m_qp[m_pq[j]];
+    SDL_ASSERT((i == j) == (pi.priority == pj.priority));
+    A_STATIC_CHECK_TYPE(value_type, pi.priority); // should be the same type
+    A_STATIC_CHECK_TYPE(value_type, pj.priority);
+    pi.priority = static_cast<value_type>(i);
+    pj.priority = static_cast<value_type>(j);
+}
+
+template<typename T, typename U, class Container>
+void priority_queue<T, U, Container>::fixUp(size_t k)
+{
+    SDL_ASSERT(check_index(k));
+    while ((k > 1) && less(k, k / 2)) {
+        exch(k, k / 2);
+        k /= 2;
+    }
+}
+
+template<typename T, typename U, class Container>
+void priority_queue<T, U, Container>::fixDown(size_t k, const size_t N)
+{
+    SDL_ASSERT(check_index(k));
+    SDL_ASSERT(check_index(N));
+    while (k <= (N / 2)) {
+        size_t j = k + k;
+        if ((j < N) && less(j + 1, j)) {
+            ++j;
+        }
+        if (!less(j, k))
+            break;
+        exch(k, j);
+        k = j;
+        SDL_ASSERT(k <= N);
+    }
+}
+
+template<typename T, typename U, class Container>
+T priority_queue<T, U, Container>::getmin()
+{
+    SDL_ASSERT(!empty());
+    const auto top = m_pq[1];
+    exch(1, size());
+    fixDown(1, size() - 1);
+    m_pq.pop_back();
+    return top;
+}
+
+template<typename T, typename U, class Container>
+void priority_queue<T, U, Container>::insert(value_type v)
+{
+    m_pq.push_back(v);
+    m_qp[v].priority = static_cast<value_type>(size());
+    fixUp(size());
+}
+
+template<typename T, typename U, class Container>
+void priority_queue<T, U, Container>::lower(value_type v)
+{
+    fixUp(m_qp[v].priority);
+}
+
+} // sdl
+
+#endif // __SDL_NUMERIC_PRIORITY_QUEUE_H__
